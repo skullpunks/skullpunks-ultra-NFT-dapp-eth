8127640c2a6dacb4a9ddad03f5ba9e44ed470007
areedgetit <forcedtopedal@gmail.com>
2022-07-08 14:46:05 -0600

    commit

--- src/App.js
+++ src/App.js
@@ -221,7 +221,7 @@ function App() {
         <s.SpacerLarge />
         
         <s.SpacerSmall />
-        <ResponsiveWrapper flex={1} style={{ padding: 24 }} test>
+        <ResponsiveWrapper flex={1} style={{ padding: 60 }} test>
           <s.Container flex={1} jc={"center"} ai={"center"}>
            </s.Container>
           <s.SpacerLarge />
@@ -237,7 +237,6 @@ function App() {
             }}
           >
             <StyledLogo alt={"logo"} src={"/config/images/logo.png"} />
-            <s.SpacerLarge />
             <s.SpacerLarge />
             <s.SpacerSmall />
             <s.TextTitle
