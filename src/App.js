be8147947e5dacf83e1011599525f7b327cda218
areedgetit <forcedtopedal@gmail.com>
2022-07-08 14:49:35 -0600

    commit

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ export const StyledButton = styled.button`
   font-weight: bold;
   font-size: 25px;
   color: var(--secondary-text);
-  width: 300px;
+  width: 275px;
   height: 50px;
   cursor: pointer;
   box-shadow: 0px 6px 0px -2px rgba(250, 250, 250, 0.3);
